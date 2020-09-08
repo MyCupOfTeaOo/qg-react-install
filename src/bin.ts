ec6e839d25aa2616b91b641a480bc7353b8230e0
zou wendi <542095059@qq.com>
2020-09-08 19:27:13 +0800

    feat: 支持重置配置文件

--- src/bin.ts
+++ src/bin.ts
@@ -10,19 +10,26 @@ import './ComRunner/bin';
 import './BlockRunner/bin';
 
 program.version(require(path.join(ctx.INSTALL_PATH, './package.json')).version);
-program.option('-c, --config', 'show local config').action(props => {
-  if (props.config) {
-    console.log('配置文件地址: ', ctx.CONFIG_PATH);
-    console.log('本地block缓存位置: ', ctx.BLOCK_PATH);
-    console.log('本地com缓存位置: ', ctx.COM_PATH);
-    console.log(
-      '本地配置: ',
-      prettier.format(fse.readFileSync(ctx.CONFIG_PATH).toString('utf-8'), {
-        parser: 'json',
-      }),
-    );
-  }
-});
+program
+  .option('-c, --config', 'show local config')
+  .option('-r, --reset', 'reset local config')
+  .action(props => {
+    if (props.reset) {
+      signale.info('重置配置文件');
+      fse.copySync(ctx.SOURCE_CONFIG, ctx.CONFIG_PATH);
+    }
+    if (props.config) {
+      console.log('配置文件地址: ', ctx.CONFIG_PATH);
+      console.log('本地block缓存位置: ', ctx.BLOCK_PATH);
+      console.log('本地com缓存位置: ', ctx.COM_PATH);
+      console.log(
+        '本地配置: ',
+        prettier.format(fse.readFileSync(ctx.CONFIG_PATH).toString('utf-8'), {
+          parser: 'json',
+        }),
+      );
+    }
+  });
 if (!fse.existsSync(ctx.CONFIG_PATH)) {
   signale.info('初始化配置文件');
   fse.copySync(ctx.SOURCE_CONFIG, ctx.CONFIG_PATH);
