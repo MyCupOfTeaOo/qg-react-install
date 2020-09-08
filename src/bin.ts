8877826cef85e9f3d80f866a5f36d281b15cd574
zou wendi <542095059@qq.com>
2020-09-08 17:07:42 +0800

    feat: 增加配置打印

--- src/bin.ts
+++ src/bin.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 import nconf from 'nconf';
+import prettier from 'prettier';
 import signale from 'signale';
 import fse from 'fs-extra';
 import ctx from './ctx';
@@ -8,10 +9,22 @@ import './ComRunner/bin';
 import './BlockRunner/bin';
 
 program.version('0.0.1');
-program.option('--config', 'show local config');
+program.option('-c, --config', 'show local config');
 if (!fse.existsSync(ctx.CONFIG_PATH)) {
   signale.info('初始化配置文件');
   fse.copySync(ctx.SOURCE_CONFIG, ctx.CONFIG_PATH);
 }
 nconf.argv().env().file({ file: ctx.CONFIG_PATH });
 program.parse(process.argv);
+if (program.config) {
+  console.log('配置文件地址: ', ctx.CONFIG_PATH);
+  console.log('本地block缓存位置: ', ctx.BLOCK_PATH);
+  console.log('本地com缓存位置: ', ctx.COM_PATH);
+  console.log(
+    '本地配置: ',
+    prettier.format(fse.readFileSync(ctx.CONFIG_PATH).toString('utf-8'), {
+      parser: 'json',
+    }),
+  );
+  process.exit();
+}
